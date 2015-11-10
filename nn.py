fed2ecc1d30588b713a5e18bcf0572793d5b7419
Zachary Wilson <zakrywilson@gmail.com>
2015-11-10 10:47:24 -0700

    Update comments and formatting in nn.py

--- nn.py
+++ nn.py
@@ -4,12 +4,12 @@
 import numpy as np
 
 # sigmoid function
-def nonlin(x,deriv=False):
-  if(deriv==True):
-    return x*(1-x)
-  return 1/(1+np.exp(-x))
+def nonlin(x, deriv=False):
+  if (deriv==True):
+    return x * (1 - x)
+  return 1 / (1 + np.exp(-x))
 
-# input dataset
+# input dataset: 12 possible key signatures
 i = np.array([ 
     [1,0,1,0,1,1,0,1,0,1,0,1], # .05: C major
     [1,1,0,1,0,1,1,0,1,0,1,0], # .10: C-sharp major
@@ -31,26 +31,22 @@ o = np.array([[.05,.10,.15,.20,.25,.30,.35,.40,.45,.50,.55,.60]]).T
 np.random.seed(1)
 
 # initialize weights randomly with mean 0
-syn0 = 2*np.random.random((12,1)) - 1
+synapse = 2 * np.random.random((12, 1)) - 1
 
 for iter in xrange(20000):
 
   # forward propagation
   layer_one = i
-  layer_two = nonlin(np.dot(layer_one,syn0))
+  layer_two = nonlin(np.dot(layer_one, synapse))
 
   # how much did we miss?
   layer_two_error = o - layer_two
 
-  # multiply how much we missed by the 
-  # slope of the sigmoid at the values in layer_two
-  layer_two_delta = layer_two_error * nonlin(layer_two,True)
+  # multiply net error by slope of the sigmoid at values in layer_two
+  layer_two_delta = layer_two_error * nonlin(layer_two, True)
 
   # update weights
-  syn0 += np.dot(layer_one.T,layer_two_delta)
-
-# because scientific notation is annoying...
-#np.set_printoptions(suppress=True)
+  synapse += np.dot(layer_one.T, layer_two_delta)
 
 # display results
 print "--------        -------"
@@ -60,9 +56,3 @@ for x, y in zip(o.tolist(), layer_two.tolist()):
   for expected, answer in zip(x, y):
     answer = round(answer, 2)
     print " ", format(expected, '.2f') ,"   -->   ", format(answer, '.2f')
-
-
-
-
-
-
